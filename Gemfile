3825a2d134207bb9aaf3894aecb4c5c96e945d6c
Keith Webber <keith.e.webber@gmail.com>
2014-04-02 20:05:15 -0400

    init commit w/ gems

---
+++ Gemfile
@@ -0,0 +1,35 @@
+source 'https://rubygems.org'
+
+gem 'devise'
+gem 'rails', '4.0.4'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.2'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'simple_form'
+gem 'foundation-rails'
+
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+group :development do
+  gem 'quiet_assets'
+end
+
+group :test, :development do
+  gem 'rspec-rails'
+  gem 'pry'
+  gem 'factory_girl_rails'
+  gem 'shoulda'
+  gem 'railroady'
+end
+
+group :test do
+  gem 'launchy'
+  gem 'capybara'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LeadMailer::Application.config.secret_key_base = 'ade6bfa9ab4bffd3fa72fc7fe8d8b8e96ac30947cae309cb7d17b178da92f344bf8a7a9d9ceec4079e6add2012b3f2f960f9a5d7eafeb3631e0e166727028830'
